48a9dff1a7cd3bc2d48d8613406ae129651d9cd9
martoast <alexmartos96@gmail.com>
2025-11-24 08:26:38 -0800

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Client-side app (SPA)
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
